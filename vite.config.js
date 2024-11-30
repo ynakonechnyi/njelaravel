ed9b2bfd3ed8e1a731e92dac45a89b3d8c2f1196
ynakonechnyi <126713681+ynakonechnyi@users.noreply.github.com>
2024-11-30 13:51:14 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                'public/resources/sass/app.scss',
+                'public/resources/js/app.js',
             ],
             refresh: true,
         }),
