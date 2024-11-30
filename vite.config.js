649e8924344143e2e21c36feb53c7c89a84d7bd2
ynakonechnyi <126713681+ynakonechnyi@users.noreply.github.com>
2024-11-30 13:13:49 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/',
+    base: '/public/',
     plugins: [
         laravel({
             input: [
