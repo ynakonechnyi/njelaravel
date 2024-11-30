3107137430e062a4bb59420acafe59804b1966e0
ynakonechnyi <126713681+ynakonechnyi@users.noreply.github.com>
2024-11-30 05:42:24 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -49,8 +49,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
-            /* 'username' => env('DB_USERNAME', 'forge'), */
-            'username' => 'if0_37818520',
+            'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
