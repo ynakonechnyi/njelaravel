c3d0966fc0ca6fbffc9000d3d450eb4715b75ddf
ynakonechnyi <126713681+ynakonechnyi@users.noreply.github.com>
2024-11-30 14:05:02 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,20 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
+   {
+       /**
+        * Register any application services.
+        *
+        * @return void
+        */
+         public function register()
+       {
+           if (env('APP_ENV') !== 'local') {
+               URL::forceScheme('https');
+           }
+       }
+   }
     public function boot(): void
     {
         //
